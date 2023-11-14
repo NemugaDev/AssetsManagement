43c47fc53dd7edcaf6d6c20e04d724ba54abf3a3
Denis Mugane <denismugane@gmailcom>
2023-11-14 14:40:56 +0300

    modified app.java class

--- src/main/java/co/assets/App.java
+++ src/main/java/co/assets/App.java
@@ -9,6 +9,10 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("the data is "+data);
+		if(data.equals("bill"))
+		{
+			System.out.println("welcome");
+		}
 	}
     public static void main( String[] args )
     {
