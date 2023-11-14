ec763ca54a8eb196ee13c2cec9d7469a0a400eed
Denis Mugane <denismugane@gmailcom>
2023-11-14 14:38:37 +0300

    Intial Commit

---
+++ src/main/java/co/assets/App.java
@@ -0,0 +1,17 @@
+package co.assets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data)
+	{
+		System.out.println("the data is "+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
